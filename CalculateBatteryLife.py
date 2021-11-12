0fc96fc14fc90a38fea76852f0a4d92dcd7c841c
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2021-11-12 20:05:54 +0000

    More precise and bettery explaning comments added, for understanding the script.

--- CalculateBatteryLife.py
+++ CalculateBatteryLife.py
@@ -11,11 +11,16 @@ PercentageMinus = 5
 
 Pr17MinutesPassed = 0
 
+# While the starting battery is higher then what your end battery goal is, it will keep going down. 
 while StartBattery > EndBattery:
+    # Here knowing what the battey goes down by the certain minute, gets taken away from the start battery.
     StartBattery -= 5
     print(f"bettery percentage is {StartBattery}.")
-    Pr17MinutesPassed += 1
+    # For this specific test where after 17 minutes the battery goes down by 5 procent at 4 % brightness -
+    # It adds 1 time to every 17 minutes that has passed so it can tell how long it took the computer to loose all battery.
+    Pr17MinutesPassed += 1 
     print(f"17 minutes has passed {Pr17MinutesPassed} times.")
 
+# Checks start battery for value 0, so it can stop and add everything together.
 if StartBattery == 0:
     print("It took ", 17 * Pr17MinutesPassed, " minutes for the battery to die while at 4 screen brightness" )
